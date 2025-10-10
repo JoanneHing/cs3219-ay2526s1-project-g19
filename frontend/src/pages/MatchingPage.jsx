50513cdc20114095831755f7e7be3412b2be12fc
JoanneHing <yenxinghing@gmail.com>
2025-10-10 23:14:56 +0800

    Add basic frontend for matching service

--- frontend/src/pages/MatchingPage.jsx
+++ frontend/src/pages/MatchingPage.jsx
@@ -1,2 +1,12 @@
-const MatchingPage = () => <h1>Matching Page</h1>;
+import MatchingForm from "../components/matchingService/matchingForm";
+
+const MatchingPage = () => {
+    return (
+        <div className="flex w-full h-screen">
+            <div className="flex items-center justify-center w-full">
+                <MatchingForm />
+            </div>
+        </div>
+    );
+};
 export default MatchingPage;
