6dd776bd4bc8834227cdc48c97c5d077901f899f
JoanneHing <yenxinghing@gmail.com>
2025-10-11 20:43:07 +0800

    Update minor changes

--- frontend/src/components/matchingService/MatchNotFound.jsx
+++ frontend/src/components/matchingService/MatchNotFound.jsx
@@ -3,11 +3,11 @@ import { XCircle, RotateCcw } from "lucide-react";
 const MatchNotFound = ({ onRematch, onReturn}) => {
 return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-        <div className = "rounded-lg shadow-lg p-8 max-w-lg w-full relative bg-background-secondary border border-gray-700">
+        <div className = "rounded-lg shadow-lg p-8 max-w-xl w-full relative bg-background-secondary border border-gray-700">
             <div className="text-center">
                 <XCircle className="mx-auto mb-4 h-12 w-12 text-red-500" />
                 <h3 className="text-gray-200 font-bold mb-2">No Match Found</h3>
-                <p className="text-gray-400 p-2 mb-6">We couldn't find a suitable match based on your preferences.</p>
+                <p className="text-gray-400 p-2">We couldn't find a suitable match based on your preferences.</p>
                 <p className="text-gray-400 p-2 mb-6">You can try adjusting your preferences or match again.</p>
 
                 <div className="flex justify-center gap-4">
