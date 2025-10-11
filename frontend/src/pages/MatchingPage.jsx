b2b88b48818d5e02a3dd110486068994ff0b4df1
JoanneHing <yenxinghing@gmail.com>
2025-10-11 20:43:07 +0800

    Update minor changes

--- frontend/src/pages/MatchingPage.jsx
+++ frontend/src/pages/MatchingPage.jsx
@@ -1,10 +1,14 @@
 import MatchingForm from "../components/matchingService/matchingForm";
+import CancelConfirmation from "../components/matchingService/CancelConfirmation";
+import MatchFound from "../components/matchingService/MatchFound";
+import MatchNotFound from "../components/matchingService/MatchNotFound";
 
 const MatchingPage = () => {
     return (
         <div className="flex w-full h-screen">
             <div className="flex items-center justify-center w-full">
                 <MatchingForm />
+                <MatchNotFound />
             </div>
         </div>
     );
