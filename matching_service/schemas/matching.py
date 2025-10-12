8547e9660bc56544a422598a889bc65cba79fc96
Shaun Lee <shaunlxw@gmail.com>
2025-10-13 01:21:55 +0800

    Add validation for criteria selection: has to be in valid list

--- matching_service/schemas/matching.py
+++ matching_service/schemas/matching.py
@@ -17,13 +17,46 @@ class MatchingCriteriaSchema(BaseModel):
     def topics_not_empty(cls, value: list[str]) -> list[str]:
         return value if len(value) > 0 else django_question_service.get_topics()
 
+    @field_validator('topics', mode='after')
+    @classmethod
+    def topics_are_valid(cls, value: list[str]) -> list[str]:
+        valid_topics = django_question_service.get_topics()
+        if len(value) > 0:
+            for topic in value:
+                if topic not in valid_topics:
+                    raise ValueError(f"Topic {topic} is not a valid topic in the list {valid_topics}")
+        return value
+
     @field_validator('difficulty', mode='after')
     @classmethod
     def difficulty_not_empty(cls, value: list[str]) -> list[str]:
         return value if len(value) > 0 else django_question_service.get_difficulty()
 
+    @field_validator('topics', mode='after')
+    @classmethod
+    def difficulty_are_valid(cls, value: list[str]) -> list[str]:
+        # skip validation for now
+        return value
+        valid_diffs = django_question_service.get_difficulty()
+        if len(value) > 0:
+            for diff in value:
+                if diff not in valid_diffs:
+                    raise ValueError(f"Difficulty {diff} is not a valid difficulty in the list {valid_diffs}")
+        return value
+
     @model_validator(mode="after")
     def language_validation(self) -> Self:
+        valid_languages = django_question_service.get_languages()
+        invalid_lang_msg = "Language {} is not a valid language in the list " + str(valid_languages)
+        if self.primary_lang is not None and self.primary_lang not in valid_languages:
+            # skip validation for now
+            # raise ValueError(invalid_lang_msg.format(self.primary_lang))
+            pass
+        for lang in self.secondary_lang:
+            if lang not in valid_languages:
+                # skip validation for now
+                # raise ValueError(invalid_lang_msg.format(lang))
+                pass
         if self.primary_lang is not None and len(self.secondary_lang) > 0:
             if self.primary_lang in self.secondary_lang:
                 raise ValueError(f"Primary language {self.primary_lang} cannot be in secondary languages list {self.secondary_lang}")
