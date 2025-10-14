107e528e6e0fe5c99d0ccbef8ae47d10ad681be8
JoanneHing <yenxinghing@gmail.com>
2025-10-14 15:45:34 +0800

    Fix pages width

--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -3,7 +3,7 @@ import NavBar from './common/NavBar';
 
 const AppLayout = () => {
   return (
-    <div className="w-full min-h-screen bg-background font-inter">
+    <div className="w-screen min-h-screen bg-background font-inter">
       <NavBar />
       <main className="w-full">
         <Outlet /> 
